7936937dee4ed5862c131ba4dad6b4dba1a88857
Shanto <Shanto@DESKTOP-32Q6JUN>
2024-08-21 00:43:38 +0600

    Add project files.

---
+++ ContactManagementAPI.Application/Requests/ContactManagement/Contacts/IContactService.cs
@@ -0,0 +1,18 @@
+﻿using ContactManagementAPI.Application.Common.Models;
+using ContactManagementAPI.Application.Requests.ContactManagement.Contacts.Queries;
+using ContactManagementAPI.Domain.Entities.ContactManagement.Contact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagementAPI.Application.Requests.ContactManagement.Contacts
+{
+    public interface IContactService : IDisposable
+    {
+        Task<Result> CreateOrUpdateContact(Contact request);
+        Task<List<Contact>> GetContactList(GetContactList request);
+        Task<Result> DeleteContact(DeleteContact request);
+    }
+}
---
+++ ContactManagementAPI.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ContactManagementAPI.Domain.Entities.ContactManagement.Contact;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagementAPI.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
